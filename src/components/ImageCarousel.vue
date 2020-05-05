32f1156edc875fb682d034e73e6f0cffe4f1bd50
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-05-05 16:09:02 +0200

    added ImageCarousel component
    Co-authored-by: <miniharald> <miniharald@gmail.com>

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+       <div class="h-50">
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="200"
+  >
+    <b-carousel-slide
+      caption="First slide"
+      :img-src="require('../assets/sweden.png')" class="carousel-img"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Second Slide"
+      :img-src="require('../assets/chidera.jpg')" class="carousel-img"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://picsum.photos/1024/480/?image=22" class="carousel-img"
+    ></b-carousel-slide>
+  </b-carousel>
+</div> 
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'ImageCarousel',
+}
+</script>
+<style scoped>
+.carousel-img{
+    height: 50vh;
+}
+
+
+</style>
