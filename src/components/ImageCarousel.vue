ea9ea90b41f26e7e78f84b20cf299437a2d07839
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-05-08 11:57:34 +0200

    stylat om bildkarusellen + några bilder
    Co-Authored-By: carljohan-hornestam <carljohan-hornestam@users.noreply.github.com>

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,27 +1,40 @@
 <template>
     <div>
-       <div class="h-100">
-  <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="200"
-  >
-    <b-carousel-slide
-      caption="First slide"
-      :img-src="require('../assets/chidera.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Second Slide"
-      :img-src="require('../assets/chimaobi.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Third Slide"
-      :img-src="require('../assets/danjuma.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-  </b-carousel>
+       <div class="w-75 h-75 m-auto">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active img1">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Nnamdi</h2>
+          <p class="lead">Fått kapitalstöd för att kunna avsluta sin utbildning. Läser internationella relationer.</p>
+        </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item img2">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Chidera</h2>
+          <p class="lead">Pojken har kämpat mot bland annat hjärtproblem sen födseln. Fått vårdstöd!</p>
+        </div>
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item img3">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Cynthia</h2>
+          <p class="lead">Fått kapitalstöd för att kunna avsluta sin utbildning. Hon läser filosofi.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
 </div> 
     </div>
 </template>
@@ -32,8 +45,42 @@ export default {
 }
 </script>
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Quicksand:wght@700&family=Noto+Sans&display=swap");
+
 .carousel-img{
     height: 75vh;
+}
+.lead {
+  background-color: #d6930d;
+  font-family: "Noto Sans", sans-serif;
+  width: inherit;
+  margin: 0 !important;
+}
+h2 {
+  font-family: "Quicksand", sans-serif;
+}
+.carousel-item {
+  height: 80vh;
+  max-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.img1 {
+  background-image: url(../assets/c4.jpg);
+}
+.img2 {
+  background-image: url(../assets/c2.jpg);
+}
+.img3 {
+  background-image: url(../assets/cynthia.jpg);
+}
+.carousel-caption {
+  left: 0 !important;
+  right:0 !important;
+  padding-bottom: 0 !important;
 }
 
 
