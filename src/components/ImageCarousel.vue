039e398616f3bf0f544d09e182a173a8af7308d8
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-05-11 13:24:39 +0200

    lagt till översättning av text på karusellen
    Co-authored-by: <miniharald> <miniharald@gmail.com>

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,54 +1,83 @@
 <template>
-    <div>
-       <div class="w-75 h-75 m-auto">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-
-    <div class="carousel-inner" role="listbox">
-      <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active img1">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Nnamdi</h2>
-          <p class="lead">Fått kapitalstöd för att kunna avsluta sin utbildning. Läser internationella relationer.</p>
+  <div>
+    <div class="w-75 h-75 m-auto">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active img1">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Nnamdi</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Fått kapitalstöd för att kunna avsluta sin utbildning. Läser internationella relationer.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >Recieved economic support in order to finish his education. He is currently studying international relations.</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item img2">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Chidera</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Pojken har kämpat mot bland annat hjärtproblem sen födseln. Fått vårdstöd.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >The boy has been fighting heart condition since he was born. Been given economic support for healthcare.</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item img3">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Cynthia</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Fått kapitalstöd för att kunna avsluta sin utbildning. Hon läser filosofi.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >Recieved economic support in order to finish her education. She is currently studying international relations.</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item img2">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Chidera</h2>
-          <p class="lead">Pojken har kämpat mot bland annat hjärtproblem sen födseln. Fått vårdstöd!</p>
-        </div>
-      </div>
-      <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item img3">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Cynthia</h2>
-          <p class="lead">Fått kapitalstöd för att kunna avsluta sin utbildning. Hon läser filosofi.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
-  </div>
-</div> 
+      </div>
     </div>
+  </div>
 </template>
 <script>
-
 export default {
-    name: 'ImageCarousel',
-}
+  name: "ImageCarousel"
+};
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Quicksand:wght@700&family=Noto+Sans&display=swap");
 
-.carousel-img{
-    height: 75vh;
+.carousel-img {
+  height: 75vh;
 }
 .lead {
   background-color: #d6930d;
@@ -79,10 +108,8 @@ h2 {
 }
 .carousel-caption {
   left: 0 !important;
-  right:0 !important;
+  right: 0 !important;
   bottom: 0 !important;
   padding-bottom: 0 !important;
 }
-
-
 </style>
