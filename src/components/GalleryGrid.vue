57f00f2d281b11cb832c64ba08639b71db4df7d3
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-05-11 15:52:42 +0200

    tog bort onödig klippt bild, fixat fel text i karusell, lagt till möjlighet att förstora bilder i galleri
    Co-Authored-By: carljohan-hornestam <carljohan-hornestam@users.noreply.github.com>

--- src/components/GalleryGrid.vue
+++ src/components/GalleryGrid.vue
@@ -2,42 +2,115 @@
   <div class="container pt-5 d-flex justify-content-center">
     <div class="bg-white pb-3 w-75 p-5" id="shadow">
       <div class="row mb-2">
-        <figure class="col grid-height push-up" id="g1">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
-        <figure class="col grid-height mx-2 push-up" id="g2">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
-        <figure class="col grid-height push-up" id="g3">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
+        <figure class="col grid-height push-up custom-link" id="g1" v-on:click="showModal('img1')">
+          <img id="img1" src="../assets/chidera.jpg" alt="Pojken har kämpat mot bland annat hjärtproblem sen födseln. Fått vårdstöd." style="display:none;" v-if="this.$store.state.isSwedish">
+          <img id="img1" src="../assets/chidera.jpg" alt="The boy has been fighting heart condition since he was born. Been given economic support for healthcare." style="display:none;" v-if="!this.$store.state.isSwedish">
+          <figcaption>
+            <p>Chidera</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height mx-2 push-up custom-link" id="g2" v-on:click="showModal('img2')">
+          <img id="img2" src="../assets/chimaobi.jpg" alt="Chimaobi" style="display:none;">
+          <figcaption>
+            <p>Chimaobi</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height push-up custom-link" id="g3" v-on:click="showModal('img3')">
+          <img id="img3" src="../assets/cynthia.jpg" alt="Fått kapitalstöd för att kunna avsluta sin utbildning. Hon läser filosofi." style="display:none;" v-if="this.$store.state.isSwedish">
+          <img id="img3" src="../assets/cynthia.jpg" alt="Recieved economic support in order to finish her education. She is currently studying philosophy." style="display:none;" v-if="!this.$store.state.isSwedish">
+          <figcaption>
+            <p>Cynthia</p>
+          </figcaption>
+        </figure>
       </div>
       <div class="row mb-2">
-        <figure class="col grid-height push-up" id="g4">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
-        <figure class="col grid-height mx-2 push-up" id="g5">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
-        <figure class="col grid-height push-up" id="g6">
-            <figcaption>
-              <p>Hamburgare</p>
-            </figcaption>
-          </figure>
+        <figure class="col grid-height push-up custom-link" id="g4" v-on:click="showModal('img4')">
+          <img id="img4" src="../assets/danjuma.jpg" alt="Danjuma" style="display:none;">
+          <figcaption>
+            <p>Danjuma</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height mx-2 push-up custom-link" id="g5" v-on:click="showModal('img5')">
+          <img id="img5" src="../assets/Handover.jpg" alt="Chinasa" style="display:none;">
+          <figcaption>
+            <p>Chinasa</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height push-up custom-link" id="g6" v-on:click="showModal('img6')">
+          <img id="img6" src="../assets/nnamdi.jpg" alt="Fått kapitalstöd för att kunna avsluta sin utbildning. Läser internationella relationer." style="display:none;" v-if="this.$store.state.isSwedish">
+          <img id="img6" src="../assets/nnamdi.jpg" alt="Recieved economic support in order to finish his education. He is currently studying international relations." style="display:none;" v-if="!this.$store.state.isSwedish">
+          <figcaption>
+            <p>Nnamdi</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="row mb-2">
+        <figure class="col grid-height push-up custom-link" id="g7" v-on:click="showModal('img7')">
+          <img id="img7" src="../assets/obi-twins.jpg" alt="Obi tvillingarna, Jane och Joy" style="display:none;">
+          <figcaption>
+            <p>Obi tvillingarna, Jane och Joy</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height mx-2 push-up custom-link" id="g8" v-on:click="showModal('img8')">
+          <img id="img8" src="../assets/favour.jpg" alt="Favour" style="display:none;">
+          <figcaption>
+            <p>Favour</p>
+          </figcaption>
+        </figure>
+        <figure class="col grid-height push-up custom-link" id="g9" v-on:click="showModal('img9')">
+          <img id="img9" src="../assets/jane.jpg" alt="Jane" style="display:none;">
+          <figcaption>
+            <p>Jane</p>
+          </figcaption>
+        </figure>
       </div>
     </div>
+    <div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close" v-on:click="closeModal()">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <div id="modal-content">hej</div>
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption">hej</div>
+</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods:{
+    showModal(imageId) {
+      let modal = document.getElementById("myModal")
+      let modalImg = document.getElementById(imageId)
+      let captionText = document.getElementById("caption")
+      let content = document.getElementById("modal-content")
+      let span = document.getElementsByClassName("close")[0]
+            console.log("modal: ", modal)
+            console.log("modalImg: ", modalImg)
+            console.log("captionText: ", captionText)
+            console.log("span: ", span)
+      modal.style.display = "block";
+      modalImg.src = document.getElementById(imageId).src
+      captionText.innerHTML = document.getElementById(imageId).alt
+      content.innerHTML = "<img class=\"myImg\" src=" + modalImg.src + ">"
+      console.log("content:", content)
+      
+    },
+    closeModal() {
+      let modal = document.getElementById("myModal")
+      modal.style.display = "none";
+    }
+  }
+};
+</script>
 
 <style scoped>
 #shadow {
@@ -113,6 +186,65 @@ figcaption p {
   color: white;
   text-align: center;
   font-size: 120%;
+}
+.custom-link {
+  border-radius: 5px;
+  cursor: pointer;
+  /*transition: 0.3s;*/
+}
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Caption of Modal Image (Image Text) - Same Width as the Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation - Zoom in the Modal */
+#modal-content, #caption {
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@keyframes zoom {
+  from {transform:scale(0)}
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
 }
 #g1 {
   background-image: url(../assets/chidera.jpg);
--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,28 +1,70 @@
 <template>
-    <div>
-       <div class="h-100">
-  <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="200"
-  >
-    <b-carousel-slide
-      caption="First slide"
-      :img-src="require('../assets/chidera.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Second Slide"
-      :img-src="require('../assets/chimaobi.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Third Slide"
-      :img-src="require('../assets/danjuma.jpg')" class="carousel-img"
-    ></b-carousel-slide>
-  </b-carousel>
-</div> 
+  <div>
+    <div class="w-75 h-75 m-auto">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active img1">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Nnamdi</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Fått kapitalstöd för att kunna avsluta sin utbildning. Läser internationella relationer.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >Recieved economic support in order to finish his education. He is currently studying international relations.</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item img2">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Chidera</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Pojken har kämpat mot bland annat hjärtproblem sen födseln. Fått vårdstöd.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >The boy has been fighting heart condition since he was born. Been given economic support for healthcare.</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item img3">
+            <div class="carousel-caption d-none d-md-block">
+              <h2 class="display-4">Cynthia</h2>
+              <p
+                class="lead"
+                v-if="this.$store.state.isSwedish"
+              >Fått kapitalstöd för att kunna avsluta sin utbildning. Hon läser filosofi.</p>
+              <p
+                class="lead"
+                v-if="!this.$store.state.isSwedish"
+              >Recieved economic support in order to finish her education. She is currently studying philosophy.</p>
+            </div>
+          </div>
+        </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </div>
 </template>
 <script>
