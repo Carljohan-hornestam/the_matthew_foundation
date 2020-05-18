f4fc8cc6c29c884cd35cd36f67f01557a3208f0d
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-05-18 09:43:03 +0200

    ändrat email i contact, och lagt till engelsk översättning Co-authored-by: <miniharald> <miniharald@gmail.com>

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,42 +1,46 @@
 <template>
-<div>
-  <div class="container pt-5 d-flex justify-content-center" id="contact_form">
-    <div class="row bg-white py-3 pb-3 w-75" id="shadow">
-      <div class="col text-left pl-4 pt-4" id="contact_info">
-        <h4 id="contactinfo_text">Kontaktinformation</h4>
-        <ul style="list-style: none; padding: 0;">
-          <li class="mb-2 mt-4">
-            <strong>Matthew Foundation</strong>
-          </li>
-          <li class="mb-2">Email</li>
-        </ul>
-      </div>
-      <div class="col text-left pl-5 pt-4" id="donate_info">
-        <h4 id="donateinfo_text">Hur du donerar</h4>
-        <ul style="list-style: none; padding: 0;">
-          <li class="mb-2 mt-4">
-            <strong>Matthew Foundation</strong>
-          </li>
-          <li class="mb-2">Bankgiro: 5373-5015</li>
-        </ul>
+  <div>
+    <div class="container pt-5 d-flex justify-content-center" id="contact_form">
+      <div class="row bg-white py-3 pb-3 w-75" id="shadow">
+        <div class="col text-left pl-4 pt-4" id="contact_info">
+          <h4 id="contactinfo_text" v-if="this.$store.state.isSwedish">Kontaktinformation</h4>
+          <h4 id="contactinfo_text" v-if="!this.$store.state.isSwedish">Contact information</h4>
+
+          <ul style="list-style: none; padding: 0;">
+            <li class="mb-2 mt-4">
+              <strong>Matthew Foundation</strong>
+            </li>
+            <li class="mb-2">
+              Email:
+              <a href="mailto:info@matthewfoundation.se">info@matthewfoundation.se</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col text-left pl-5 pt-4" id="donate_info">
+          <h4 id="donateinfo_text" v-if="this.$store.state.isSwedish">Hur du donerar</h4>
+          <h4 id="donateinfo_text" v-if="!this.$store.state.isSwedish">How to donate</h4>
+          <ul style="list-style: none; padding: 0;">
+            <li class="mb-2 mt-4">
+              <strong>Matthew Foundation</strong>
+            </li>
+            <li class="mb-2">Bankgiro: 5373-5015</li>
+          </ul>
+        </div>
       </div>
     </div>
+    <ContactForm />
   </div>
-          <ContactForm />
-</div>
 </template>
 
 
 
 <script>
-import ContactForm from '@/components/ContactForm.vue';
+import ContactForm from "@/components/ContactForm.vue";
 export default {
-  name: 'Contact',
+  name: "Contact",
   components: {
-    ContactForm,
-  
-  
-},
+    ContactForm
+  },
   data() {
     return {};
   }
@@ -44,7 +48,6 @@ export default {
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Quicksand:wght@700&family=Noto+Sans&display=swap");
-
 
 #shadow {
   box-shadow: 3px 3px 3px #565756;
@@ -62,5 +65,8 @@ export default {
 }
 #contactinfo_text {
   font-family: "Quicksand", sans-serif;
+}
+a {
+  color: #d6930d !important;
 }
 </style>
